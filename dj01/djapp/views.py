0692d689f2e8baa4453cea0c95eb10cca884d7ce
Elsbelgy <mohamed.nhassan20@gmail.com>
2023-05-16 20:12:56 +0300

    first commit

---
+++ dj01/djapp/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+from .models import Students_models1
+from .models import Courses
+
+def student_list(request):
+    student_details = Students_models1.objects.all()
+    return render(request,'students.html',{'std':student_details})
+
+
+def Courses_list(request):
+    courses_details = Courses.objects.all()
+    return render(request,'courses.html',{'crs':courses_details})
